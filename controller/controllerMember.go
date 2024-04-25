25ed49616cf62afab6502b70d1a03bf83deefdbf
Ismaindra <imismaindra@gmail.com>
2024-04-25 14:17:51 +0700

    update validasi input member

--- controller/controllerMember.go
+++ controller/controllerMember.go
@@ -1,7 +1,16 @@
 package controller
 
-func InsertMember(nama string, uname string, pass string, role string, status int) {
-	if nama != "" && uname != "" && pass != "" && role != "" && status != -1 {
+import "errors"
 
+func InsertMember(nama string, uname string, pass string, role string, status int) error {
+	// Validasi input
+	if nama == "" || uname == "" || pass == "" || role == "" {
+		return errors.New("semua parameter harus diisi")
 	}
+
+	// Validasi status
+	if status <= 0 || status >= 1 {
+		return errors.New("status harus berada dalam rentang 0-1")
+	}
+	return nil
 }
